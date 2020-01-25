fe8e200dabadcc69d7e76fd80ee7826cfbd2ae76
Donald A C Martin <donald@martin.scot>
2020-01-25 08:25:42 -0500

    Read from CSV instead

--- src/main/java/scot/martin/apollo/Main.java
+++ src/main/java/scot/martin/apollo/Main.java
@@ -1,12 +1,14 @@
 package scot.martin.apollo;
 
-import scot.martin.apollo.function.transformer.ArgsShowFunction;
 import scot.martin.apollo.function.transformer.TimeTilBroadcastFunction;
+import scot.martin.apollo.io.input.ShowSupplier;
 import scot.martin.apollo.model.Show;
 import scot.martin.apollo.predicate.UpcomingShowPredicate;
 import scot.martin.apollo.worker.Downloader;
 
-import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collection;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
@@ -14,6 +16,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.logging.Logger;
 
 public class Main {
@@ -24,37 +27,32 @@ public class Main {
     private static final int POOL_SIZE = 10;
 
     public static void main(String[] args) {
-        if (args.length < 5) {
-            System.out.println("Usage: Name Time Timezone Duration URL");
+        if (args.length < 1) {
+            System.out.println("Usage: PATH_TO_CSV");
             System.exit(-1);
         }
 
-        Function<List<String>, Show> argsShowFunction = new ArgsShowFunction();
+        Path csvPath = Paths.get(args[0]);
+        Supplier<Collection<Show>> showSupplier = new ShowSupplier(csvPath);
+        Collection<Show> shows = showSupplier.get();
+
         Predicate<Show> upcomingShowPredicate = new UpcomingShowPredicate(SCHEDULER_SLEEP);
         Function<Show, Long> timeTilBroadcastFunction = new TimeTilBroadcastFunction();
         ScheduledExecutorService executorService = Executors.newScheduledThreadPool(POOL_SIZE);
 
         try {
-            Show show = argsShowFunction.apply(List.of(args));
-
-            if (show == null) {
-                executorService.shutdown();
-                System.out.println("Couldn't parse show");
-                System.exit(-1);
-            }
-
             while (true) {
-                LOGGER.info("Checking if show can be scheduled");
+                LOGGER.info("Checking if shows can be scheduled");
 
-                if (upcomingShowPredicate.test(show)) {
-                    LOGGER.info("Show " + show.getName() + " can be scheduled");
+                shows.parallelStream().filter(upcomingShowPredicate).forEach(s -> {
+                    LOGGER.info("Show " + s.getName() + " can be scheduled");
 
-                    Callable<Optional<String>> downloader = new Downloader(show);
-                    long delayMillis = timeTilBroadcastFunction.apply(show);
+                    Callable<Optional<String>> downloader = new Downloader(s);
+                    long delayMillis = timeTilBroadcastFunction.apply(s);
 
                     executorService.schedule(downloader, delayMillis, TimeUnit.MILLISECONDS);
-                    LOGGER.info("Show " + show.getName() + " scheduled");
-                }
+                    LOGGER.info("Show " + s.getName() + " scheduled");
+                });
 
                 LOGGER.info("Scheduler sleeping...");
                 Thread.sleep(SCHEDULER_SLEEP);
---
+++ src/main/java/scot/martin/apollo/io/input/ShowSupplier.java
@@ -0,0 +1,57 @@
+package scot.martin.apollo.io.input;
+
+import scot.martin.apollo.function.transformer.ArgsShowFunction;
+import scot.martin.apollo.model.Show;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+public class ShowSupplier implements Supplier<Collection<Show>> {
+
+    private static final Logger LOGGER = Logger.getLogger("ShowSupplier");
+    private static final String SEPARATOR = ",";
+
+    private final Function<List<String>, Show> argsShowFunction;
+    private final Function<String, List<String>> csvRowSplitFunction;
+    private final Path path;
+
+    public ShowSupplier(Path path) {
+        this.argsShowFunction = new ArgsShowFunction();
+        this.csvRowSplitFunction = r -> List.of(r.split(SEPARATOR));
+        this.path = path;
+    }
+
+    @Override
+    public Collection<Show> get() {
+        Collection<Show> shows = new HashSet<>();
+        File file = path.toFile();
+
+        if (file.exists() && file.canRead()) {
+            try {
+                shows = Files
+                        .lines(path)
+                        .map(csvRowSplitFunction)
+                        .map(argsShowFunction)
+                        .collect(Collectors.toSet());
+
+                LOGGER.info("Read " + shows.size() + " shows");
+                shows.stream().forEach(x -> LOGGER.info(x.toString()));
+            } catch (IOException io) {
+                LOGGER.severe("Failed to read shows: " + io.getMessage());
+            }
+        } else {
+            LOGGER.severe("File doesn't exist or is unreadable");
+        }
+
+        return shows;
+    }
+}
