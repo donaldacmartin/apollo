173c9a07a6835574f3243b1625fecb7256e52a61
Donald AC Martin <donald@donald-martin.ca>
2021-10-09 07:45:54 -0400

    Only keep last 5 files

--- cartierville/rss.py
+++ cartierville/rss.py
@@ -6,6 +6,8 @@ Functions:
     update_rss(str, PodcastDTO, str, int, int) -> None
 """
 from email.utils import formatdate
+from os import listdir, remove
+from os.path import basename, dirname, join
 from time import gmtime, strftime
 from uuid import uuid4
 from xml.etree.ElementTree import ElementTree, SubElement, parse, register_namespace
@@ -13,6 +15,7 @@ from xml.etree.ElementTree import ElementTree, SubElement, parse, register_names
 from cartierville.model import PodcastDTO, RSSException
 
 ITUNES_NAMESPACE = "http://www.itunes.com/dtds/podcast-1.0.dtd"
+MAX_KEEP_ITEMS = 5
 
 
 def _serialise_duration(duration: int) -> str:
@@ -44,6 +47,36 @@ def _add_item(
     _add_enclosure(item, link, file_size)
 
 
+def _remove_old_items(channel: ElementTree) -> None:
+    items = channel.findall("item")
+    num_to_delete = max(0, len(items) - MAX_KEEP_ITEMS)
+    elements_to_delete = items[:num_to_delete]
+
+    for element in elements_to_delete:
+        channel.remove(element)
+
+
+def _delete_files_not_in_list(channel: ElementTree, rss_location: str) -> None:
+    rss_dir = dirname(rss_location)
+    rss_file = basename(rss_location)
+    dir_files = set(listdir(rss_dir))
+
+    items = channel.findall("item")
+    links = [item.find("link").text for item in items]
+    expected_filenames = set([link.split("/")[-1] for link in links] + [rss_file])
+
+    files_to_delete = list(dir_files.difference(expected_filenames))
+    paths_to_delete = [join(rss_dir, filename) for filename in files_to_delete]
+
+    for path in paths_to_delete:
+        remove(path)
+
+
+def _clean_up(channel: ElementTree, rss_location: str) -> None:
+    _remove_old_items(channel)
+    _delete_files_not_in_list(channel, rss_location)
+
+
 def update_rss(
     rss_location: str, podcast: PodcastDTO, link: str, duration: int, file_size: int
 ) -> None:
@@ -54,6 +87,7 @@ def update_rss(
         root = xml.getroot()
         channel = root.find("channel")
         _add_item(channel, podcast, link, duration, file_size)
+        _clean_up(channel, rss_location)
         xml.write(rss_location)
     except Exception as exception:
         raise RSSException(exception) from exception
